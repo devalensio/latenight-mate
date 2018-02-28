2e6636c02fba444dfd4c1fd84d8b099dcd5d9db3
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-02-28 13:45:22 +0700

    crud genre

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const user = require('./routes/user')
+const genre = require('./routes/genre')
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/users', user)
+app.use('/genres', genre)
 
 app.listen(3000, console.log('hahahahahihihi'))
---
+++ models/genre.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Genre = sequelize.define('Genre', {
+    name: DataTypes.STRING
+  }, {});
+  Genre.associate = function(models) {
+    // associations can be defined here
+  };
+  return Genre;
+};
